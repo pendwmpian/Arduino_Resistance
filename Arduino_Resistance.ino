d01819a738f3ee71b37cb01af47760a006d6342e
pendwmpian <22346581+pendwmpian@users.noreply.github.com>
2023-11-15 18:44:51 +0900

    rewrite arduino I2C comm with adc

--- Arduino_Resistance.ino
+++ Arduino_Resistance.ino
@@ -1,81 +1,75 @@
-#include <Adafruit_ADS1X15.h>
+#include <Arduino.h>
+#include <Wire.h>
 
-Adafruit_ADS1115 ads;  /* Use this for the 16-bit version */
-// Adafruit_ADS1015 ads;     /* Use this for the 12-bit version */
+#define SEND_ERROR_TO_SERIAL Serial.println("Failed to connect with device.")
+#define SLAVE_ADDR 0x48
 
 union Data {
-  struct __attribute__((__packed__)) {
-    uint32_t time;
-    int16_t volt;
-    uint8_t chk_sum = 0;
-  };
-  uint8_t bin[7];
+    struct __attribute__((__packed__)) {
+        uint32_t time;
+        int16_t volt;
+        uint8_t chk_sum = 0;
+    };
+    uint8_t bin[7];
 };
 
 void encodeCOBS(Data* dat, uint8_t* encoded){
-  dat->chk_sum = 0;
-  for(uint8_t i = 0; i < sizeof(Data) - 1; i++){
-    dat->chk_sum += dat->bin[i];
-  }
-  encoded[sizeof(Data) + 1] = 0;
-  uint8_t zero = 1;
-  for(int8_t i = sizeof(Data) - 1; i >= 0; i--){
-    if(dat->bin[i] != 0) encoded[i + 1] = dat->bin[i];
-    else {encoded[i + 1] = zero; zero = 0;}
-    zero++;
-  }
-  encoded[0] = zero;
+    dat->chk_sum = 0;
+    for(uint8_t i = 0; i < sizeof(Data) - 1; i++){
+        dat->chk_sum += dat->bin[i];
+    }
+    encoded[sizeof(Data) + 1] = 0;
+    uint8_t zero = 1;
+    for(int8_t i = sizeof(Data) - 1; i >= 0; i--){
+        if(dat->bin[i] != 0) encoded[i + 1] = dat->bin[i];
+        else {encoded[i + 1] = zero; zero = 0;}
+        zero++;
+    }
+    encoded[0] = zero;
 }
 
 
 
 void setup(void)
 {
-  Serial.begin(115200);
+    Serial.begin(115200);
 
-  Serial.println("ADC Range: +/- 6.144V (1 bit = 3mV/ADS1015, 0.1875mV/ADS1115)");
-
-  // The ADC input range (or gain) can be changed via the following
-  // functions, but be careful never to exceed VDD +0.3V max, or to
-  // exceed the upper and lower limits if you adjust the input range!
-  // Setting these values incorrectly may destroy your ADC!
-  //                                                                ADS1015  ADS1115
-  //                                                                -------  -------
-  ads.setGain(GAIN_TWOTHIRDS);  // 2/3x gain +/- 6.144V  1 bit = 3mV      0.1875mV (default)
-  // ads.setGain(GAIN_ONE);        // 1x gain   +/- 4.096V  1 bit = 2mV      0.125mV
-  // ads.setGain(GAIN_TWO);        // 2x gain   +/- 2.048V  1 bit = 1mV      0.0625mV
-  // ads.setGain(GAIN_FOUR);       // 4x gain   +/- 1.024V  1 bit = 0.5mV    0.03125mV
-  // ads.setGain(GAIN_EIGHT);      // 8x gain   +/- 0.512V  1 bit = 0.25mV   0.015625mV
-  // ads.setGain(GAIN_SIXTEEN);    // 16x gain  +/- 0.256V  1 bit = 0.125mV  0.0078125mV
-
-  ads.setDataRate(RATE_ADS1115_860SPS);
-
-  if (!ads.begin()) {
-    while (1){
-        Serial.println("Failed to initialize ADS.");
-    }
-  }
+    Wire.begin();
+    Wire.beginTransmission(SLAVE_ADDR);
+    Wire.write(0x01);
+    Wire.write(0x40);
+    Wire.write(0xE3);
+    if(Wire.endTransmission()) SEND_ERROR_TO_SERIAL;
 }
 
 
 void loop(void)
 {
-  Data result;
-  uint8_t encoded[sizeof(Data) + 2];
+    Data result;
+    uint8_t encoded[sizeof(Data) + 2];
 
-  /* Be sure to update this value based on the IC and the gain settings! */
-  //float   multiplier = 3.0F;    /* ADS1015 @ +/- 6.144V gain (12-bit results) */
-  //float multiplier = 0.1875F; /* ADS1115  @ +/- 6.144V gain (16-bit results) */
-  //float R0 = 390.0F;
-  //float gain = 1.0F + (100000.0F / R0);
+    uint8_t conversion_ready = 0x00;
+    while(conversion_ready & 0x80 != 0x00){
+        Wire.beginTransmission(SLAVE_ADDR);
+        Wire.write(0x01);
+        Wire.endTransmission();
+        Wire.requestFrom(SLAVE_ADDR, 2);
+        while(Wire.available()){
+            conversion_ready = Wire.read();
+            Wire.read();
+        }
+    } 
 
-  result.volt = ads.readADC_SingleEnded(0);
-  //Serial.println(micros());
-  result.time = micros();
-  encodeCOBS(&result, encoded);
-  for(uint8_t i = 0; i < sizeof(encoded); i++) Serial.write(encoded[i]);
+    Wire.beginTransmission(SLAVE_ADDR);
+    Wire.write(0x00);
+    Wire.endTransmission();
+    Wire.requestFrom(SLAVE_ADDR, 2);
+    while(Wire.available()){
+        result.volt = Wire.read() << 8;
+        result.volt += Wire.read() & 0x00FF;
+    }
 
-  //Serial.println(micros()); //Serial.print(" "); Serial.println(String(results * multiplier / gain, 6));
-
-  //delay(20);
+    result.time = micros();
+    encodeCOBS(&result, encoded);
+    for(uint8_t i = 0; i < sizeof(encoded); i++) Serial.write(encoded[i]);
 }
